fe142403cd0ff5b0b2c0300389583dd8ed1c8424
Chucks Nwachukwu <chucks@bbd.co.za>
2020-08-01 14:45:58 +0200

    Building Apps Using Xamarin

---
+++ Building-Xamarin/README.md
@@ -0,0 +1,3 @@
+# Building-Xamarin.Forms-Mobile-App-Using-XAML (2019)
+__By Dan Hermes__  
+[Example Code](https://github.com/danhermes/xamarin-xaml-book-examples)  
---
+++ Building-Xamarin/chp1/FormsExample/FormsExample/FormsExample/ContentPageExample.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Threading.Tasks;
+
+namespace FormsExample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ContentPageExample : ContentPage
+	{
+		public ContentPageExample()
+		{
+			InitializeComponent();
+		}
+
+		protected void ButtonClicked(object sender, EventArgs args)
+		{
+			((Button)sender).Text = "You 've got it";
+		}
+
+		protected void ImageTapped(object sender, EventArgs args)
+		{
+			Image image = ((Image)sender);
+			image.Source = image.Source.ToString() == "logo.png" ? ImageSource.FromFile("logo_black.png") : ImageSource.FromFile("logo.png"); // Does not work fully.
+		}
+
+		async protected void PhotoImageTapped(object sender, EventArgs args)
+		{
+			Image image = (Image)sender;
+			/*
+			image.Opacity = 0.1;
+			await Task.Delay(200);
+			image.Opacity = 1;
+			*/
+
+			await image.FadeTo(0.5, 450);
+			await Task.Delay(200);
+			await image.FadeTo(1, 450);
+		}
+	}
+}
