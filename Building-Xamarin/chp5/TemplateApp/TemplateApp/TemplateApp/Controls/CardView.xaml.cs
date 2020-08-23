42e019d9dab3043388f4cba2376b027629d57cf0
Chucks Nwachukwu <chucks@bbd.co.za>
2020-08-23 15:32:49 +0200

    Custom Control And Templates

---
+++ Building-Xamarin/chp5/TemplateApp/TemplateApp/TemplateApp/Controls/CardView.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace TemplateApp.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CardView : ContentView
+	{
+		public CardView()
+		{
+			InitializeComponent();
+		}
+
+		public static readonly BindableProperty CardTitleProp = BindableProperty.Create("CardTitle", typeof(string), typeof(CardView), "Default Title");
+		public static readonly BindableProperty CardDescriptionProp = BindableProperty.Create("CardDescription", typeof(string), typeof(CardView), "Default Description");
+		public static readonly BindableProperty IconImageSourceProp = BindableProperty.Create("IconImageSource", typeof(ImageSource), typeof(CardView));
+		public static readonly BindableProperty IconBackgroundColorProp = BindableProperty.Create("IconBackgroundColor", typeof(Color), typeof(CardView), Color.Green);
+		public static readonly BindableProperty BorderColorProp = BindableProperty.Create("BorderColor", typeof(Color), typeof(CardView), Color.Blue);
+		public static readonly BindableProperty CardColorProp = BindableProperty.Create("CardColor", typeof(Color), typeof(CardView), Color.Yellow);
+
+
+
+		public string CardTitle
+		{
+			get => (string)GetValue(CardView.CardTitleProp);
+			set => SetValue(CardView.CardTitleProp, value);
+		}
+
+		public string CardDescription
+		{
+			get => (string)GetValue(CardView.CardDescriptionProp);
+			set => SetValue(CardView.CardDescriptionProp, value);
+		}
+
+		public ImageSource IconImageSource
+		{
+			get => (ImageSource)GetValue(CardView.IconImageSourceProp);
+			set => SetValue(CardView.IconImageSourceProp, value);
+		}
+
+		public Color IconBackgroundColor
+		{
+			get => (Color)GetValue(CardView.IconBackgroundColorProp);
+			set => SetValue(CardView.IconBackgroundColorProp, value);
+		}
+
+		public Color BorderColor
+		{
+			get => (Color)GetValue(CardView.BorderColorProp);
+			set => SetValue(CardView.BorderColorProp, value);
+		}
+			
+		public Color CardColor
+		{
+			get => (Color)GetValue(CardView.CardColorProp);
+			set => SetValue(CardView.CardColorProp, value);
+		}
+	}
+
+}
---
+++ Building-Xamarin/chp5/TemplateApp/TemplateApp/TemplateApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using TemplateApp.Controls;
+
+namespace TemplateApp
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			//Todo: Learn hw to use the control in the xaml page
+			CardView AnotherCard = new CardView
+			{
+				BorderColor = Color.DarkGray,
+				CardTitle = "Slavko Vlasic",
+				CardDescription = "Test Custom View",
+				IconBackgroundColor = Color.SlateGray,
+				IconImageSource = ImageSource.FromFile("photo.png")
+			};
+		}
+	}
+}
