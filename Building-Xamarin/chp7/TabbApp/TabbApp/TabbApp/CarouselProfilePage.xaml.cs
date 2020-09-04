bbd11053da24b4fff7d95fa3ebeeaf451ada4b41
Chucks Nwachukwu <chucks@bbd.co.za>
2020-09-04 22:31:06 +0200

    Navigation using Tabbed Page or Carousel Page

---
+++ Building-Xamarin/chp7/TabbApp/TabbApp/TabbApp/CarouselProfilePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CarouselProfilePage : CarouselPage
+	{
+		public CarouselProfilePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Building-Xamarin/chp7/TabbApp/TabbApp/TabbApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbApp
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			/* Using the tap gesture recognizer */
+			TapGestureRecognizer tapRecognizer = new TapGestureRecognizer();
+			tapRecognizer.Tapped += async (sender, evt) =>
+			{
+				// For visual responsive effect
+				Image image = (Image)sender;
+				image.Opacity = 0.5;
+				await Task.Delay(500);
+				image.Opacity = 1;
+
+				// Now navigation to the page
+				await Navigation.PushAsync(new ProfilePage());
+			};
+			ProfileImage.GestureRecognizers.Add(tapRecognizer);
+		}
+
+		private async void GotoPage(object sender, EventArgs e)
+		{
+			string pageStr = (string)((Button)sender).CommandParameter;
+			Type type = Type.GetType($"TabbApp.{pageStr}");
+			Page page = (Page)Activator.CreateInstance(type);
+			await Navigation.PushAsync(page);
+		}
+	}
+}
