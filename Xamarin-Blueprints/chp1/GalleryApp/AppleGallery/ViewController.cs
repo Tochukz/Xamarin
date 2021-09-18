7fc907435375072759df26fd3c8606bde351d2de
Tochukwu Nwachukwu <tochukz@EpicDealss-MacBook-Pro.local>
2021-09-18 22:22:23 +0200

    Gallery App

---
+++ Xamarin-Blueprints/chp1/GalleryApp/AppleGallery/ViewController.cs
@@ -0,0 +1,52 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using CoreGraphics;
+using AppleGallery.DataSources;
+
+namespace AppleGallery
+{
+
+    public partial class ViewController : UIViewController
+    {
+        private UITableView tableView;
+
+        private TableSource tableSource;
+
+        private ImageHandler imageHandler;
+
+        public ViewController(IntPtr handle) : base(handle)
+        { 
+            tableSource = new TableSource();
+            imageHandler = new ImageHandler();
+            imageHandler.AssetsLoaded += handleAssetsLoaded;
+
+        }
+
+        private void handleAssetsLoaded(object sender, EventArgs e)
+        {
+            tableSource.UpdateGalleryItems(imageHandler.CreateGalleryItems());
+            tableView.ReloadData();
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            var width = View.Bounds.Width;
+            var height = View.Bounds.Height;
+
+            tableView = new UITableView(new CGRect(0, 0, width, height));
+            tableView.AutoresizingMask = UIViewAutoresizing.All;
+            tableView.Source = tableSource;
+
+            Add(tableView);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
