d74ba07748ce6b7d0e6c7a4789fff4fd838ff852
Chucks Nwachukwu <chucks@bbd.co.za>
2021-12-07 22:42:45 +0200

    Curvy App

---
+++ CurvyApp/CurvyApp/CurvyApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CurvyApp
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			Device.SetFlags(new string[] { "Shapes_Experimental"});
+
+			InitializeComponent();
+
+			//MainPage = new MainPage();
+			MainPage = new NavigationPage(new MainPage());
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ CurvyApp/README.md
@@ -0,0 +1,4 @@
+## Usefull Links 
+
+[Drawing UI with Xamarin.Forms Shapes and Paths](https://devblogs.microsoft.com/xamarin/xamarin-forms-shapes-and-paths/)
+[Xamarin.Forms: How to Clip Images with Rounded Corners](https://montemagno.com/xamarin-forms-how-to-clip-images-with-rounded-corners/)
